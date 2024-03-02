88a9d7f0fccceab2257d8ada92d3eccb200cf064
AdeilH <madeel2344@gmail.com>
2024-03-02 19:35:28 +0000

    Tried videos stitching

---
+++ videos_stitching.py
@@ -0,0 +1,19 @@
+clips1 = [[0, 1], [2, 6], [8, 10], [1, 9],[1, 5], [5, 9]]
+clips1_time = 10
+
+clips2 = [[0,1],[1,2]]
+clips2_time = 5
+
+clips3 = [[0,1],[6,8],[0,2],[5,6],[0,4],[0,3],[6,7],[1,3],[4,7],[1,4],[2,5],[2,6],[3,4],[4,5],[5,7],[6,9]]
+clips3_time = 9
+
+def videos_stitching(clips , time) -> int:
+    sol = 0
+    sorted_clips = sorted(clips)
+    for i in sorted_clips:
+        
+    return sol
+
+videos_stitching(clips1, clips1_time)
+videos_stitching(clips2, clips2_time)
+videos_stitching(clips3, clips3_time)
